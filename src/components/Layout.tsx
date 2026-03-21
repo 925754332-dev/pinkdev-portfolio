4fd643b6b51526bb397c0c5213d315cfa753cab7
xiaoyi <xiaoyi@example.com>
2026-03-21 12:44:34 +0800

    fix: adjust logo icon size and spacing

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -13,7 +13,7 @@ const Layout = () => {
       <header className="header">
         <Link to="/" className="logo" onClick={() => setMobileMenuOpen(false)}>
           <div className="logo-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="28" height="28">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="44" height="44">
               <defs>
                 <linearGradient id="logoGrad" x1="0%" y1="0%" x2="100%" y2="100%">
                   <stop offset="0%" stopColor="#FF85A1"/>
